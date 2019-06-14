3a93ffe7e8f04eea05d847e911a048a9b155035f
Y.A.K.E <xjw1162@gmail.com>
2019-06-14 22:47:33 +0800

    Update 弹琴吧VIP播放脚本.user.js

--- 弹琴吧VIP播放脚本.user.js
+++ 弹琴吧VIP播放脚本.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         弹琴吧VIP播放脚本
 // @namespace    https://www.yge.me/
-// @version      0.1
+// @version      0.2
 // @description  需要安装他们的客户端,调用客户端播放的.
 // @author       Y.A.K.E
 // @match        http://www.tan8.com/yuepu-*.html
@@ -14,7 +14,7 @@
     // Your code here...
 
     if ('javascript:;' ==  jQuery("#btn_play").attr('href')){
-        var vhref ='/open_yp.php?ypid' + ypid;
+        var vhref ='/open_yp.php?ypid=' + ypid;
         jQuery("#btn_play").attr('href',vhref ).attr('target',"_blank").html('免VIP播放').removeAttr('id');
     }
 })();
